f80244d0dc7fb81ecfc55d82ed75cd0d9820c02a
George Charles <charlesschiedel@gmail.com>
2019-07-21 18:45:46 +0300

    I have created the Credentials class

--- password.py
+++ password.py
@@ -14,8 +14,25 @@ class User:
         self.first_name = first_name
         self.last_name = last_name
         self.password = password
-                                        def save_user(self):
+                                        
+    def save_user(self):
         """
         Function to save user login details
         """
         User.user_list.append(self)
+
+        
+class Credentials:
+    """
+    Class to define credentials for the different accounts
+    """
+    credentials_list = []
+
+    def __init__(self,account_name,username,password):
+        """
+        Function th initialize credential fields correctly
+        """
+
+        self.account_name = account_name
+        self.username = username
+        self.password = password
